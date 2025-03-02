efe3d3e35f5184e722a1d73a4d6613241a18343d
rony539 <143897221+rony5394@users.noreply.github.com>
2025-03-02 11:30:50 +0100

    Still Working

--- example/RequestHandler.php
+++ example/RequestHandler.php
@@ -5,13 +5,39 @@ namespace Rony539\PhpFramework\Example;
 class RequestHandler{
 	static public function LoadDataById(){
 		$requestBody = json_decode(file_get_contents('php://input'));
-		$requestedDataId = $requestBody["id"];
 
-		if(!isset($requestedDataId)){http_response_code(400);exit;}
+		if(!isset($requestBody->id)){http_response_code(400);exit;}
+
+		if(!is_file(TEST_DB_PATH)){http_response_code(404);exit;}
+
+		$requestedDataId = $requestBody->id;
+
 		if(!isset($testDbData[$requestedDataId])){http_response_code(404);exit;};
 
-		$testDbData = json_decode(file_get_contents(__DIR__."/../testing.json"));
+		$testDbData = json_decode(file_get_contents(TEST_DB_PATH));
 
 		echo json_encode(["data"=>$testDbData[$requestedDataId]]);
+	}
+
+	static public function SaveData(){
+		$requestBody = json_decode(file_get_contents('php://input'));	
+		if(!isset($requestBody->data)){http_response_code(400);exit;}
+
+		if(!is_file(TEST_DB_PATH)){
+			file_put_contents(TEST_DB_PATH, "{}");
+		}
+
+		$newDataId = uniqid(more_entropy: true);
+
+		$dbData = json_decode(file_get_contents(TEST_DB_PATH));
+
+		$isWriteSuccessfull = file_put_contents(TEST_DB_PATH,
+			
+			[$newDataId] = $requestBody->data, LOCK_EX
+		);
+
+		if(!$isWriteSuccessfull){http_response_code(507);exit;}
+
+		echo json_encode(["id"=>$newDataId]);
 	}
 }
