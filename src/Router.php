937a8a44cb75ecaa6c69195efe6db21b14b7e79b
rony539 <143897221+rony5394@users.noreply.github.com>
2025-03-06 21:10:06 +0100

    Above under and then beyond
    There is so much fixed stuff i didn't saw before and...
    Added Middleware.
    Implemented in Example using loadSecretData.
    Fixed Toolkit $requestJsonBodyParsed,
    checkObjectForm(...) edgecase
    I'll write test cases in next commit

--- src/Router.php
+++ src/Router.php
@@ -3,11 +3,17 @@ namespace Rony539\PhpFramework;
 require_once __DIR__."/../vendor/autoload.php";
 
 class Router {
-	// I could make it private but there will no use for protected
+	// I could make it private but...
 	protected $routes = [];
+	protected $middlewares = [];
 
-	public function route(string $httpMethod, string $httpPath, callable $handler): void{
-		$this->routes[$httpPath][$httpMethod] = $handler;
+	public function route(string $httpMethod, string $httpPath, callable $handler, array $middlewares = []): void{
+		$this->routes[$httpPath][$httpMethod]["handler"] = $handler;
+		$this->routes[$httpPath][$httpMethod]["middlewares"] = $middlewares;
+	}
+
+	public function middleware(string $name, callable $handler): void{
+		$this->middlewares[$name] = $handler;
 	}
 
 	public function dispatch(string $requestedHttpMethod, string $requestedHttpPath): int {
@@ -16,7 +22,15 @@ class Router {
 		if(!isset($this->routes[$requestedHttpPath][$requestedHttpMethod]))return 405;
 
 		ob_start();
-		$response_code = $this->routes[$requestedHttpPath][$requestedHttpMethod]();
+			foreach ($this->routes[$requestedHttpPath][$requestedHttpMethod]["middlewares"] as $middlewareName) {
+				$middlewareCallable = $this->middlewares[$middlewareName];
+
+				$response_code = $middlewareCallable();
+				if($response_code)return $response_code;
+			}
+
+
+		$response_code = $this->routes[$requestedHttpPath][$requestedHttpMethod]["handler"]();
 
 		if(!is_int($response_code)){
 			throw new \Exception("Route $requestedHttpMethod '$requestedHttpPath' did not returned status code!");
